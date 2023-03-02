d385f8bb9d880e428d048ce3c13b6ae7565b0ecb
HabtB <email.Habtamu@gmail.com>
2023-03-02 18:13:56 +0000

    9 updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,18 +15,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-	else
-		left_height = 0;
-
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	
 	if (tree->right)
 		right_height = 1 + binary_tree_height(tree->right);
 	else
 		right_height = 0;
 
-	if (left_height > right_height)
-		return (left_height);
-	else
-		return (right_height);
+	return (left_height > right_height ? left_height: right_height);
 }
