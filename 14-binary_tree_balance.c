c149ca2749ccf90dfac099e36a88286304634b1a
HabtB <email.Habtamu@gmail.com>
2023-03-03 17:32:30 +0000

    14 added

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+	}
+	return ((l > r) ? l : r);
+}
+
+/**
 * binary_tree_balance - measures the balance factor of binary tree
 *
 * @tree: is a pointer to the root of the tree to measure the balance factor
@@ -9,16 +30,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_left = 0;
-	int height_right = 0;
+	int left = 0;
+	int right = 0;
+	int total = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left)
-		height_left = 1 + binary_tree_balance(tree->left);
-	if (tree->right)
-		height_right = 1 + binary_tree_balance(tree->right);
-
-	return (height_left - height_right);
+	if (tree != NULL)
+	{
+		left = (int)binary_tree_height_b(tree->left);
+		right = (int)binary_tree_height_b(tree->right);
+		total = left - right;
+	}
+	return (total);
 }
