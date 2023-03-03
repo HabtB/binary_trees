241b8be804133cbba2f726fc1ea716d00eea9855
HabtB <email.Habtamu@gmail.com>
2023-03-03 09:43:59 +0000

    10 updated comply to c90 stds and remove mixed declarations

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,10 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t node_depth = 0;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t node_depth = 0;
 
 	if (tree->parent)
 		node_depth = 1 + binary_tree_depth(tree->parent);
