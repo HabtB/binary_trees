8755413cf6f8f38a894b71333fcaa0ab0f2345f0
HabtB <email.Habtamu@gmail.com>
2023-03-03 18:10:12 +0000

    13-number of nodes updated

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,11 +11,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (1);
-	else
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left || tree->right)
+		nodes++;
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
 }
