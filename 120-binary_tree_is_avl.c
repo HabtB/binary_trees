47bd4ffae820ef63574f381bc9994f415abdb823
HabtB <email.Habtamu@gmail.com>
2023-03-04 11:21:44 +0000

    120 binary is avl is updated

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,21 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_avl - finds if a binary tree is an avl
- * @tree: pointer to the root node of the tree
- *
- * Return: 1 if tree is avl
- *         0 otherwise
- */
-int binary_tree_is_avl(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (btia_helper(tree, INT_MIN, INT_MAX));
-}
-
-/**
  * btia_helper - helper that finds if a binary tree is an avl
  * @tree: pointer to the root node of the tree
  * @min: minimum value
@@ -63,3 +48,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
 }
+
+/**
+ * binary_tree_is_avl - finds if a binary tree is an avl
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if tree is avl
+ *         0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (btia_helper(tree, INT_MIN, INT_MAX));
+}
+
